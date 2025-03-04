5528f3b39b55b1d0f2c2caa2e8cd618be9418fb6
16211994 <cakek433@gmail.com>
2025-03-04 16:34:16 +0300

    Added feature to save quotes to a text file

--- scraper.py
+++ scraper.py
@@ -8,12 +8,14 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
     quotes = soup.find_all("div", class_="quote")
 
-    for quote in quotes:
-        text = quote.find("span", class_="text").get_text()
-        author = quote.find("small", class_="author").get_text()
-        tags = [tag.get_text() for tag in quote.find_all("a", class_="tag")]
+    with open("quotes.txt", "w") as file:
+        for quote in quotes:
+            text = quote.find("span", class_="text").get_text()
+            author = quote.find("small", class_="author").get_text()
+            tags = [tag.get_text() for tag in quote.find_all("a", class_="tag")]
 
-        print(f"Quote: {text}\nAuthor: {author}\nTags: {', '.join(tags)}\n")
+            file.write(f"Quote: {text}\nAuthor: {author}\nTags: {', '.join(tags)}\n\n")
 
+    print("Quotes have been saved to quotes.txt.")
 else:
     print("Failed to retrieve the webpage.")
