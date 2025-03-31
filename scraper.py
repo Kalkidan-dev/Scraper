43785843750dd9a2dd5b405a0d0e152fcf43cba7
16211994 <cakek433@gmail.com>
2025-03-31 21:47:13 +0300

    Added language detection for quotes.

--- scraper.py
+++ scraper.py
@@ -816,7 +816,22 @@ for quote in quotes_list:
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """, (quote['text'], quote['author'], quote['author_url'], "N/A", "N/A", ", ".join(quote['tags']), quote['scrape_time'], quote['sentiment'], quote['length'], quote['word_count'], quote['popularity_score'], quote['source'], quote['is_duplicate']))
     conn.commit()
+def detect_language(text):
+    """Detect the language of the quote."""
+    try:
+        return detect(text)
+    except Exception:
+        return "Unknown"
 
+# Update data collection
+for quote in quotes_list:
+    quote['language'] = detect_language(quote['text'])
+
+    cursor.execute("""
+        INSERT INTO quotes (text, author, author_url, birth_date, birth_place, tags, scrape_time, sentiment, length, word_count, popularity_score, source, language)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (quote['text'], quote['author'], quote['author_url'], "N/A", "N/A", ", ".join(quote['tags']), quote['scrape_time'], quote['sentiment'], quote['length'], quote['word_count'], quote['popularity_score'], quote['source'], quote['language']))
+    conn.commit()
 
 # End time tracking and display execution time
 end_time = time.time()
