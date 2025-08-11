4a1c275532258e27c748af13aac899973a27c714
Kalkidan-dev <cakek433@gmail.com>
2025-08-11 09:32:27 +0300

    Edited

--- web_scraper.py
+++ web_scraper.py
@@ -347,7 +347,7 @@ def extract_open_graph_data(soup):
     
     return "\n".join(og_data)
 
-# Function to extract phone numbers from the page
+
 def extract_phone_numbers(soup):
     text = soup.get_text()
     phone_numbers = re.findall(r'\+?\d[\d\-\(\) ]{7,}\d', text)
