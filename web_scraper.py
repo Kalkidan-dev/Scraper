d35272be4f8959cb3fa04438b16c6c877b5237f0
Kalkidan-dev <cakek433@gmail.com>
2025-08-11 09:33:02 +0300

    Edited

--- web_scraper.py
+++ web_scraper.py
@@ -348,6 +348,10 @@ def extract_open_graph_data(soup):
     return "\n".join(og_data)
 
 
+def extract_phone_numbers(soup):
+    text = soup.get_text()
+    phone_numbers = re.findall(r'\+?\d[\d\-\(\) ]{7,}\d', text)
+    return "\n".join(set(phone_numbers)) if phone_numbers else "No phone numbers found"
 
 def detect_language(soup):
     html_tag = soup.find('html')
